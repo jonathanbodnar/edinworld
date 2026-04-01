71d5f15de3f5fdaf802dbcaceed9d5d460284e56
Jonathan Bodnar <jonathanbodnar@gmail.com>
2026-04-01 11:19:37 -0500

    Use separate alembic_version_world table to avoid collision with System A

--- alembic/env.py
+++ alembic/env.py
@@ -12,6 +12,9 @@ if config.config_file_name is not None:
 target_metadata = Base.metadata
 
 
+VERSION_TABLE = config.get_main_option("version_table", "alembic_version_world")
+
+
 def run_migrations_offline() -> None:
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
@@ -19,6 +22,7 @@ def run_migrations_offline() -> None:
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
+        version_table=VERSION_TABLE,
     )
     with context.begin_transaction():
         context.run_migrations()
@@ -31,7 +35,11 @@ def run_migrations_online() -> None:
         poolclass=pool.NullPool,
     )
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+            version_table=VERSION_TABLE,
+        )
         with context.begin_transaction():
             context.run_migrations()
 
